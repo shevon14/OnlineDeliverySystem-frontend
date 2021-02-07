34cc9c2938ae41a32fd44cf6920116fb8405c6d9
Darshana Herath <gihankck15@gmail.com>
2021-02-07 13:21:01 +0530

    map data

--- src/app/admin/user-chart/user-delivers/user-delivers.component.ts
+++ src/app/admin/user-chart/user-delivers/user-delivers.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-import { DeliversDetailsService } from '../../../services/delivers.service';
+import { DeliversDetailsService, DeliversDetails } from '../../../services/delivers.service';
 
 @Component({
   selector: 'app-user-delivers',
   templateUrl: './user-delivers.component.html',
   styleUrls: ['./user-delivers.component.scss']
 })
+
+
 export class UserDeliversComponent implements OnInit {
   @ViewChild ('frame') public formModal: ElementRef;
 
   delivers:any;
+  deliversData:any;
+  full_name:string;
+  email:string;
+  conatct:string;
+  drivingLicenceId:string;
+  vehicaleLicenceNumber:string;
   contentdeliver:any;
   openPopup:boolean =false;
   constructor( private deliversDetailsService:DeliversDetailsService) { }
@@ -26,17 +34,23 @@ export class UserDeliversComponent implements OnInit {
   )
   }
 
-  moreData(email:string){
-    this.openPopup=true
-    this.deliversDetailsService.getADeliversDetails(email).subscribe((res) => {
-      this.delivers=res;
-      console.log(res)
-    },
+  moreData(index){
+    this.openPopup=true;
+    this.deliversData=(this.delivers[index]);
+    this.full_name=this.deliversData.full_name;
+    this.email=this.deliversData.email;
+    this.conatct=this.deliversData.conatct;
+    this.drivingLicenceId=this.deliversData.drivingLicenceId;
+    this.vehicaleLicenceNumber=this.deliversData.vehicaleLicenceNumber;
+    // this.deliversDetailsService.getADeliversDetails(email).subscribe((res) => {
+    //   this.delivers=res;
+      console.log("res")
+  //   },
 
-    err => {
-      console.error(err)
-    }
-  )
+  //   err => {
+  //     console.error(err)
+  //   }
+  // )
 }
     
 
